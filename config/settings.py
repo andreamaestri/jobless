daebdfc8874e93ec3a8b0471a11e7444075d67c3
Andrea Maestri <andreamaestri.home@gmail.com>
2024-12-18 11:46:53 +0000

    Add authentication settings and update auto-save preferences; clean up CSS styles

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -150,8 +155,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_USERNAME_REQUIRED = True
-
+ACCOUNT_USERNAME_REQUIRED = False
+LOGIN_REDIRECT_URL = '/' 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
