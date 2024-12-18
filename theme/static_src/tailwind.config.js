3f129e58a10694ea451b3f26864483df29cb9397
Andrea Maestri <andreamaestri.home@gmail.com>
2024-12-18 09:05:00 +0000

    Add 'Jobless Dark' theme configuration to Tailwind CSS

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -47,42 +47,80 @@ module.exports = {
         themes: [
             {
                 'Jobless': {
-                    'primary' : '#e5855d',
-                    'primary-focus' : '#ca5516',
-                    'primary-content' : '#f5f0eb',
+                    'primary': '#e5855d',
+                    'primary-focus': '#ca5516',
+                    'primary-content': '#f5f0eb',
 
-                    'secondary' : '#cb581a',
-                    'secondary-focus' : '#a04718',
-                    'secondary-content' : '#f5f0eb',
+                    'secondary': '#cb581a',
+                    'secondary-focus': '#a04718',
+                    'secondary-content': '#f5f0eb',
 
-                    'accent' : '#9c3116',
-                    'accent-focus' : '#be3513',
-                    'accent-content' : '#f5f0eb',
+                    'accent': '#9c3116',
+                    'accent-focus': '#be3513',
+                    'accent-content': '#f5f0eb',
 
-                    'neutral' : '#34322f',
-                    'neutral-focus' : '#ebddd0',
-                    'neutral-content' : '#ebe7e2',
+                    'neutral': '#34322f',
+                    'neutral-focus': '#ebddd0',
+                    'neutral-content': '#ebe7e2',
 
-                    'base-100' : '#fff4eb',
-                    'base-200' : '#ffede2',
-                    'base-300' : '#d8bdab',
-                    'base-content' : '#34322f',
+                    'base-100': '#fff4eb',
+                    'base-200': '#ffede2',
+                    'base-300': '#d8bdab',
+                    'base-content': '#34322f',
 
-                    'info' : '#ec8751',
-                    'success' : '#f2a982',
-                    'warning' : '#b88b75',
-                    'error' : '#bd6b56',
+                    'info': '#ec8751',
+                    'success': '#f2a982',
+                    'warning': '#b88b75',
+                    'error': '#bd6b56',
 
-                    '--rounded-box': '1rem',          
-                    '--rounded-btn': '.5rem',        
-                    '--rounded-badge': '1.9rem',      
+                    '--rounded-box': '1rem',
+                    '--rounded-btn': '.5rem',
+                    '--rounded-badge': '1.9rem',
 
-                    '--animation-btn': '.25s',       
-                    '--animation-input': '.2s',       
+                    '--animation-btn': '.25s',
+                    '--animation-input': '.2s',
 
-                    '--btn-text-case': 'uppercase',   
-                    '--navbar-padding': '.5rem',      
-                    '--border-btn': '1px',            
+                    '--btn-text-case': 'uppercase',
+                    '--navbar-padding': '.5rem',
+                    '--border-btn': '1px',
+                },
+                'Jobless Dark': {
+                    'primary': '#ff8e52',
+                    'primary-focus': '#e26c2c',
+                    'primary-content': '#34322f',
+
+                    'secondary': '#ce5412',
+                    'secondary-focus': '#8a3d14',
+                    'secondary-content': '#34322f',
+
+                    'accent': '#b71a1a',
+                    'accent-focus': '#cb471a',
+                    'accent-content': '#0d0d0d',
+
+                    'neutral': '#34322f',
+                    'neutral-focus': '#1b1a18',
+                    'neutral-content': '#ebe7e2',
+
+                    'base-100': '#0d0d0c',
+                    'base-200': '#404040',
+                    'base-300': '#5b5e71',
+                    'base-content': '#ebe7e2',
+
+                    'info': '#db7139',
+                    'success': '#ee9768',
+                    'warning': '#9a4619',
+                    'error': '#a51d1d',
+
+                    '--rounded-box': '1rem',
+                    '--rounded-btn': '.5rem',
+                    '--rounded-badge': '1.9rem',
+
+                    '--animation-btn': '.25s',
+                    '--animation-input': '.2s',
+
+                    '--btn-text-case': 'uppercase',
+                    '--navbar-padding': '.5rem',
+                    '--border-btn': '1px',
                 },
             },
             "lofi",
