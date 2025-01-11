bba387abca52529fd0cbe3986d40b3d946b41750
Andrea <16215183+andreamaestri@users.noreply.github.com>
2025-01-11 21:37:43 +0000

    Add Tailwind CSS configuration, update requirements, and implement custom signup form

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  purge: [
+    './templates/**/*.html',
+    './basic_django/utils.py',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
